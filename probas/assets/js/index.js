385974a667edef4e11ec027bd8c2a4c5e3bf77f7
marialalin <mariavencesantorum@gmail.com>
2022-06-04 10:36:45 +0200

    added lights

--- probas/assets/js/index.js
+++ probas/assets/js/index.js
@@ -2,34 +2,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const switcher = document.querySelector(".switcher")
 
 
-
     switcher.addEventListener("click", function () {
         
-        document.querySelectorAll("#bulb-container img").forEach((image) => {
+        document.querySelectorAll("#bulb-container img")
+        .forEach((image) => {
     
             if (image.className.includes("visible")){
                 image.classList.add("oculto")
                 image.classList.remove("visible")
-                document.querySelector('.switcher').classList.add("rotation")
-                /*const audio = document.createElement("audio")
-                audio.setAttribute("src")
-                audio.play()*/
-                
+                document.querySelector('.switcher')
+                .classList.remove("rotation")
+                //setInterval(stop)
                 
             } else {
                 image.classList.add("visible")
                 image.classList.remove("oculto")
-                document.querySelector('.switcher').classList.remove("rotation")
-               
+                document.querySelector('.switcher')
+                .classList.add("rotation")
+                //setInterval
+            
             } 
-        })
-        
 
-    
+        })    
     })
 
+})
 
-    /*switcher.addEventListener("click", function () {
+
+
+
+ 
+    /*
+    SEN FOREACH
+    
+    switcher.addEventListener("click", function () {
         const bombilla = document.querySelector("#bulb-container img")
         if (bombilla.className.includes("visible")) {
             bombilla.classList.add("oculto")
@@ -42,13 +48,10 @@ document.addEventListener('DOMContentLoaded', () => {
     })*/
 
 
-
-
-})
-
-
-
- /*  if ().) {
+/*  
+ CON HIDDEN/VISIBLE
+ 
+ if ().) {
             document.getElementById("bulb-container").style.visibility = 'hidden'
             console.log('heeeu')
         } else {
@@ -57,7 +60,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-/*const switcher = document.getElementById("switcher")
+/*
+CON REMOVE-CHILD. MÓVESE O HTML
+const switcher = document.getElementById("switcher")
 
 switcher.addEventListener("click", function () {
     const div = document.getElementById("bulb-container")
@@ -66,3 +71,9 @@ switcher.addEventListener("click", function () {
     }
     console.log("hey")
 })*/
+
+
+//AUDIO
+ /*const audio = document.createElement("audio")
+                audio.setAttribute("src")
+                audio.play()*/
---
+++ probas/assets/js/swicher.js
@@ -0,0 +1,9 @@
+class Swither {
+    constructor() {
+
+    }
+
+
+
+    
+}
